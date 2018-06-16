32f5f32b2807e1bcacf875367a9d551c07f8a6ba
Bingyao Liu <Bingyao.Liu@gmail.com>
2018-06-17 00:08:40 +0800

    Add argparse and initiate list command

---
+++ usermanager/um.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import pwd
+import argparse
+from pprint import pprint
+
+
+def list_users(verbose):
+    users = pwd.getpwall()
+    users.sort(key=lambda user: user[2])
+    if verbose:
+        print('You have following users on your system:')
+    pprint(users)
+
+
+parser = argparse.ArgumentParser(prog='um',
+    description='A more informative and modern user manager for Linux/Unix')
+
+group = parser.add_mutually_exclusive_group()
+group.add_argument('-l', '--list', dest='action', action='store_const', 
+                   const='list', help='list the users on the system')
+
+parser.add_argument('-v', '--verbose', action='store_true',
+                    help='increase output verbosity')
+
+args = parser.parse_args()
+
+if args.action is 'list':
+    list_users(args.verbose)
